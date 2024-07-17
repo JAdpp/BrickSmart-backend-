493820d09279415876ebeb887b3f253c650b2dd5
JAdpp <747835493@qq.com>
2024-07-17 19:53:24 +0800

    update

--- brick_packagelist.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `model3d_packagelist` (`pid`, `caption`, `description`, `createTime_Internal`, `alterTime_Internal`) VALUES (1, 'test1', NULL, '2024-06-27 03:09:12.844412', '2024-06-27 03:09:12.844412');
