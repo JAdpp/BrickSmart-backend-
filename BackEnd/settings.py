785dbd0a60b226489fa1b846ee02b56640408734
JAdpp <747835493@qq.com>
2024-08-09 16:53:57 +0800

    Add files via upload

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/data/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'data')
+MEDIA_ROOT = os.path.join(BASE_DIR.parent,'')
