7ef77ed34362bf09d2bda5ac042087a9368d4186
JAdpp <747835493@qq.com>
2024-08-09 19:04:10 +0800

    Add files via upload

--- model3d/urls.py
+++ model3d/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import generate_model, generate_model_image
+from .views import generate_model, generate_model_image, generate_lego_tutorial
 
 urlpatterns = [
     path("model/", generate_model, name='generate_model'),
     path("model/image/", generate_model_image, name='generate_model_image'),
+    path('lego_tutorial/', generate_lego_tutorial, name='generate_lego_tutorial')
 ]
 
 # 在开发环境下添加媒体文件的服务
